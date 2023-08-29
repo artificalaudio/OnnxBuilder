0f4eeae7ca830d04bacc2a2163261d1dbc2911a5
artificalaudio <135162720+artificalaudio@users.noreply.github.com>
2023-08-29 20:57:17 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "core/session/onnxruntime_cxx_api.h"
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "hello world" << std::endl;
+
+    return EXIT_SUCCESS;
+}
